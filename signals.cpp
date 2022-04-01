276be8d1f4e4219d4d057437ef860fedb851afcc
TalMalchi <malchital@gmail.com>
2022-04-01 16:59:39 +0300

    init

--- signals.cpp
+++ signals.cpp
@@ -10,51 +10,80 @@ using namespace std;
 
 void handler(int signum)
 {
-	int x=6;
-	int y=0;
+	int x = 6;
+	int y = 0;
 	int z;
 
-    switch(signum){
-		case SIGCHLD:
-			cout << "received SIGCHLD\n" << endl;
-			fflush(stdout);
-			z= x/6;
-		case SIGFPE:
-			cout << "received SIGFPE\n" << endl;
-			fflush(stdout);
-			cout << "Press ^C\n" << endl;
-		case SIGTERM:
-			cout << "received SIGTERM\n" << endl;
-			fflush(stdout);
-			alarm(3);
+	switch (signum)
+	{
+	case SIGCHLD:
+		cout << "received SIGCHLD\n"
+			 << endl;
+		fflush(stdout);
+		z = x / y;
 
-		case SIGALRM:
-			cout << "received SIGTERM\n" << endl;
-			fflush(stdout);
-			exit(1);
-		
-		//case SIGCHLD:
-		
-	}
-}
-	
-	
+	case SIGFPE:
+		cout << "received SIGFPE\n"
+			 << endl;
+		fflush(stdout);
+		abort();
+		cout << "Press ^C\n"
+			 << endl;
+			 sleep(10);
+			 cout << "\n" <<endl;
+	case SIGABRT:
+		cout << "received SIGABRT\n"
+			 << endl;
+		fflush(stdout);
+		alarm(3);
 
+	case SIGALRM:
+		cout << "received SIGALRM\n"
+			 << endl;
+		fflush(stdout);
+		raise(SIGUSR1);
+
+	case SIGUSR1:
+		cout << "received SIGUSR1\n" <<endl;
+		
+		fflush(stdout);
+		//system("lsd"); //instead ls
+		cout << "\n"<< endl;
+		exit(1);
+	
+	// // case SIGILL:
+	// // cout << "received SIGILL\n" <<endl;
+	// // 	fflush(stdout);
+	// // 	//exit(1);
+	// // 	system("^C");
+	
+	// case SIGTERM:
+	// 	cout << "received SIGTERM\n" <<endl;
+	// 	fflush(stdout);
+	// 	exit(1);
+
+	default :
+		exit(1);
+}}
 
 int main()
 {
 	int status;
-	signal (SIGCHLD, handler);
-	signal (SIGFPE, handler);
-	signal (SIGUSR1, handler);
-    signal(SIGINT, handler);
-    signal(SIGTERM, handler);
-    signal(SIGKILL, handler); 
+	signal(SIGCHLD, handler);
+	signal(SIGFPE, handler);
+	signal(SIGABRT, handler);
 	signal(SIGALRM, handler);
+	signal(SIGUSR1, handler);
+	// signal(SIGILL, handler);
+	// signal(SIGTERM, handler);
+	
+	
+	
 
-	if (!(fork())) {
+	if (!(fork()))
+	{
 		exit(1);
 	}
 	wait(&status);
-return 0; 
+	return 0;
 }
